0a9d94c6e44ee592df436acd59bf321b471434ea
Feras_shadid <Feras_shadid@yahoo.com>
2021-11-24 10:04:35 +0200

    day8

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER\Github3\22.Day7-Django-Repeat\ReactDjangoBootCamp\env
+set VIRTUAL_ENV=C:\Users\USER\Github3\24.Day8-Django-Repeat\ReactDjangoBootCamp\env
 
 if not defined PROMPT set PROMPT=$P$G
 
